aa9efe1a2b726c55cce6da9a79ec89c3ddf65a60
Jose Gutierrez <gutierrez.jose24@gmail.com>
2023-08-03 07:03:30 -0500

    phase 2 lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+  <h2>About Me</h2>
+  <p>Write anything you want to show about yourself here.</p>
+  <img src={image} alt="I made this" />
+</div>;
 }
 
 export default About;
